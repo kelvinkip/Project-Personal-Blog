60c11576c6500dabe87d6b7e59467135aa6a3d2e
kelvinkip <kelvinkipla@gmail.com>
2022-05-18 15:02:29 +0300

    created a progress

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config:
+
+    SECRET_KEY = 'SECRET_KEY'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:manage@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    #  email configurations
+    MAIL_SERVER = 'kelvinkipla@gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
+   
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:manage@localhost/blog'
+
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
